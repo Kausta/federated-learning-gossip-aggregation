72fe1931ae2ea10d55bb6440bcc6f02f5af2b978
Arda_Öztaşkın <arda@goksal.com.tr>
2020-06-01 23:39:53 +0300

    Added simple compile script for python and simple demos for rpc client server

--- communication/rpc_client.py
+++ communication/rpc_client.py
@@ -1,5 +1,28 @@
 import grpc
 import communication_pb2_grpc
+import communication_pb2
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = communication_pb2_grpc.CommunicatorStub(channel)
+SERVER_ADDRESS = 'localhost:50051'
+
+def send_model(data: bytes):
+    with grpc.insecure_channel(SERVER_ADDRESS) as channel:
+        stub = communication_pb2_grpc.CommunicatorStub(channel)
+        response = stub.SendModel(communication_pb2.Model(data=data))
+
+    return response
+
+def get_updates():
+    with grpc.insecure_channel(SERVER_ADDRESS) as channel:
+        stub = communication_pb2_grpc.CommunicatorStub(channel)
+        response = stub.ReceiveUpdates(communication_pb2.Reply())
+
+    print(response)
+    return response
+
+
+if __name__ == '__main__':
+    send_model(bytes(3))
+    send_model(bytes(1))
+    send_model(bytes(7))
+    send_model(bytes(9))
+    get_updates()
---
+++ communication/rpc_server.py
@@ -0,0 +1,60 @@
+from concurrent import futures
+import logging
+
+import grpc
+
+import communication_pb2
+import communication_pb2_grpc
+from queue import Queue, Empty
+
+
+class Server(communication_pb2_grpc.CommunicatorServicer):
+    def __init__(self, peers):
+        self.peers = peers
+
+    received_updates = Queue()
+    sending_updates = Queue()
+
+    def SendModel(self, request, context):
+        """Call RPC Server's SendModel to send the model to RPC Server
+        """
+        self.received_updates.put(request.data)
+        return communication_pb2.Reply(result="Success")
+
+    def UpdateModel(self, request, context):
+        """Gets model from python and routes to a peer. Should queue if no peer exists
+        """
+        if len(self.peers) > 0:
+            print("HEY")
+        self.sending_updates.put(request.data)
+        return communication_pb2.Reply(result="Success")
+
+    def ReceiveUpdates(self, request, context):
+        """Send update. TODO: change to streaming to process efficiently in the future
+        """
+        result = communication_pb2.Model()
+        for model in drain(self.received_updates):
+            print(model)
+        return result
+
+
+def serve():
+    port = "50051"
+    peers = []
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    communication_pb2_grpc.add_CommunicatorServicer_to_server(Server(peers), server)
+    server.add_insecure_port('[::]:{}'.format(port))
+    server.start()
+    server.wait_for_termination()   # Comment this out for non-waiting start
+
+def drain(q):
+    while True:
+        try:
+            yield q.get_nowait()
+        except Empty:
+            break
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
